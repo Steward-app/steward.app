b3039b1ce7b383fd0daf633fe8a5ba3890cade01
Artanicus <artanicus@nocturnal.fi>
2021-01-07 21:57:24 +0200

    Stop adding env to service tags

--- app/channels.py
+++ app/channels.py
@@ -35,10 +35,10 @@ class Channels():
     
     def resolve_all(self, env='dev'):
         if FLAGS.consul:
-            self.user = self.resolve('registry.user.{env}'.format(env=env))
-            self.maintenance = self.resolve('registry.maintenance.{env}'.format(env=env))
-            self.asset = self.resolve('registry.asset.{env}'.format(env=env))
-            self.schedule = self.resolve('registry.schedule.{env}'.format(env=env))
+            self.user = self.resolve('registry.user'
+            self.maintenance = self.resolve('registry.maintenance'
+            self.asset = self.resolve('registry.asset'
+            self.schedule = self.resolve('registry.schedule'
         logging.info('Resolved channels (env={env}):\n user_server: {user}\n maintenance_server: {maintenance}\n asset_server: {asset}\n schedule_server: {schedule}'.format(
             env=env,
             user=self.user,
