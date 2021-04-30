dbdb4c15d43772fafd72f1a90802e333519d683f
Artanicus <artanicus@nocturnal.fi>
2021-04-30 20:47:08 +0300

    Stop accepting consul ip & port, instead --consul is now a bool flag.
    This means that consul is assumed to be resolvable over the system
    default DNS setup. It vastly simplifes everything though in multi server
    consul DCs.

--- app/channels.py
+++ app/channels.py
@@ -7,8 +7,7 @@ from proto.steward import registry_pb2_grpc
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('consul', None, 'Define ip address to enable Consul service disovery. A hostname will not work.')
-flags.DEFINE_integer('consul_port', 8600, 'Consul port')
+flags.DEFINE_bool('consul', None, 'Enable Consul discovery of backends via DNS.')
 flags.DEFINE_string('consul_service', 'steward-registry', 'Consul service')
 flags.DEFINE_string('monolithic_host', 'localhost', 'Host to use for monolithic deployments')
 flags.DEFINE_integer('monolithic_port', 50050, 'Port to use for monolithic deployments')
@@ -28,8 +27,6 @@ class Channels():
         if FLAGS.consul:
             logging.info('Using Consul host: {host}'.format(host=FLAGS.consul))
             self.resolver = resolver.Resolver()
-            self.resolver.port = FLAGS.consul_port
-            self.resolver.nameservers = [FLAGS.consul]
         else:
             for service, stub in services.items():
                 self.uri[service] = '{}:{}'.format(FLAGS.monolithic_host, FLAGS.monolithic_port)
@@ -53,8 +50,8 @@ class Channels():
     def _resolve(self, tag):
         if FLAGS.consul:
             address = 'registry.{tag}.{service}.service.consul.'.format(tag=tag, service=FLAGS.consul_service)
-            srv = self.resolver.query(address, 'SRV')[0]
-            ip = self.resolver.query(srv.target, 'A')[0]
+            srv = self.resolver.resolve(address, 'SRV')[0]
+            ip = self.resolver.resolve(srv.target, 'A')[0]
             return '{host}:{port}'.format(host=ip, port=srv.port)
         else:
             return self.monolithic
